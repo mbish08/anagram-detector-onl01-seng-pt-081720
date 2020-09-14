fb878ba0dec7629a1fc58a3916eed39e9e4ab09c
Mikel Bishop <mdrew8@gmail.com>
2020-09-14 01:10:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,14 +2,14 @@ class Anagram
   
 attr_accessor :word 
 
-def initialize
+def initialize(word)
   @word = word 
 end 
 
-def match 
-  
-  
-  
+def match(word_array)
+  word_array.select.each do |new_word|
+  @word.split("").sort == new_word.split("").sort 
+end 
 end 
 
 end 
